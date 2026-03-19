e351ca8c2f5cc2d55b89f96c6615a293fe279323
Mitch Ivin <mitchellivin@gmail.com>
2026-03-20 08:29:18 +1000

    v0.0.3

--- api/index.js
+++ api/index.js
@@ -1,5 +1,10 @@
 const { addonBuilder, getRouter } = require('stremio-addon-sdk')
 const express = require('express')
+const path = require('path')
+
+const BASE_URL = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:7000'
 
 const A1X_SOURCES = [
   'https://bit.ly/a1xstream',
@@ -42,7 +47,50 @@ const EXCLUDED_CHANNELS = new Set([
   'Loupe 4K', 'Travelxp 4K', 'Love Nature 4K'
 ])
 
-// Which A1X groups map to which of our 3 categories
+// Map A1X channel names to local logo IDs
+const CHANNEL_LOGO_MAP = {
+  'SKY Sport 1 NZ': '403251',
+  'SKY Sport 2 NZ': '403256',
+  'SKY Sport 3 NZ': '403252',
+  'SKY Sport 4 NZ': '403248',
+  'SKY Sport 5 NZ': '403249',
+  'SKY Sport 6 NZ': '403250',
+  'Sky Sports+ FHD': '412959',
+  'Sky Sports Premier League FHD': '6958',
+  'Sky Sports News FHD': '6626',
+  'TNT Sports 1 FHD': '400477',
+  'TNT Sports 2 FHD': '400480',
+  'TNT Sports 3 FHD': '400479',
+  'TNT Sports 4 FHD': '400478',
+  'BBC News FHD': '12162',
+  'GB News FHD': '12133',
+  'Sky News FHD': '12069',
+  'BBC One FHD': '486880',
+  'BBC Two FHD': '486673',
+  'BBC Three / CBBC FHD': '486878',
+  'ESPN HD': '465198',
+  'ESPN News HD': '417108',
+  'ESPN2 HD': '465373',
+  'ESPNU HD': '417125',
+  'NBA TV': '406263',
+  'NFL Network': '454083',
+  'Animal Planet FHD': '465310',
+  'Cartoon Network FHD': '464853',
+  'Comedy Central FHD': '464922',
+  'Discovery Channel FHD': '465364',
+  'HBO Comedy FHD': '464953',
+  'Nickelodeon FHD': '465251',
+  'truTV FHD': '464987',
+  'TNT FHD': '465114',
+  'CNBC': '464791',
+  'NewsMax': '464863'
+}
+
+function getLogoUrl(channelName, fallbackLogo) {
+  const id = CHANNEL_LOGO_MAP[channelName]
+  if (id) return `${BASE_URL}/logos/${id}.png`
+  return fallbackLogo || undefined
+}
 const GROUP_TO_CATEGORY = {
   'NZ Sports': 'Sports',
   'AU Sports': 'Sports',
@@ -155,20 +203,25 @@ const manifest = {
   description: 'Live sports, entertainment and news via A1X',
   resources: ['catalog', 'meta', 'stream'],
   types: ['tv'],
-  catalogs: [{
-    type: 'tv',
-    id: 'a1x-catalog',
-    name: 'A1X IPTV',
-    extra: [{ name: 'genre', options: CATEGORIES, isRequired: true }]
-  }],
+  catalogs: [
+    { type: 'tv', id: 'a1x-sports', name: 'Sports' },
+    { type: 'tv', id: 'a1x-entertainment', name: 'Entertainment' },
+    { type: 'tv', id: 'a1x-news', name: 'News' }
+  ],
   idPrefixes: ['a1x-']
 }
 
 const builder = new addonBuilder(manifest)
 
 builder.defineCatalogHandler(async ({ type, id, extra }) => {
-  if (type !== 'tv' || id !== 'a1x-catalog') return { metas: [] }
-  const category = extra?.genre || 'Sports'
+  if (type !== 'tv') return { metas: [] }
+  const categoryMap = {
+    'a1x-sports': 'Sports',
+    'a1x-entertainment': 'Entertainment',
+    'a1x-news': 'News'
+  }
+  const category = categoryMap[id]
+  if (!category) return { metas: [] }
   try {
     const { byCategory } = await getCache()
     const channels = byCategory[category] || []
@@ -177,8 +230,8 @@ builder.defineCatalogHandler(async ({ type, id, extra }) => {
         id: ch.id,
         type: 'tv',
         name: ch.name,
-        logo: ch.logo || undefined,
-        poster: ch.logo || undefined,
+        logo: getLogoUrl(ch.name, ch.logo),
+        poster: getLogoUrl(ch.name, ch.logo),
         posterShape: 'square',
         genres: [ch.category]
       }))
@@ -223,6 +276,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   next()
 })
+app.use('/logos', express.static(path.join(__dirname, '..', 'logos')))
 app.use(getRouter(builder.getInterface()))
 
 module.exports = app
